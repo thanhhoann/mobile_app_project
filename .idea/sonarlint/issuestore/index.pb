85f04d09a834b7187d09c59faf4365a9df545703
thanhhoann <pdth456@gmail.com>
2024-10-22 22:20:15 +0700

    refactor(gradle): change kotlin & kotlin compiler version

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ P
  app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
 <
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
+p
+@app/src/main/java/com/mobile_app/project/FirebaseAuthActivity.kt,3/2/32d1ca1e743a91211701d4ee39d54d15ca4eb80c
---
+++ app/src/main/java/com/mobile_app/project/FirebaseAuthActivity.kt
@@ -0,0 +1,38 @@
+package com.mobile_app.project
+
+import android.app.Activity
+
+class FirebaseAuthActivity : Activity() {
+
+    // ...
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
+    // ...
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            Log.d(TAG, "Got ID token.")
+                        }
+
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+                }
+            }
+        }
+        // ...
+    }
